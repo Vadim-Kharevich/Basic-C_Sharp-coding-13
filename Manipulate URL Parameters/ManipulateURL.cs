f507c6b39f34b56bc60b1ba245986a7a1aad46e1
Vadim-Kharevich <xarevich99@gmail.com>
2019-02-23 11:26:55 +0300

    Add Files

---
+++ Manipulate URL Parameters/ManipulateURL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulate_URL_Parameters
+{
+    public class ManipulateURL
+    {
+        public static string addOrChangeUrlParameter(string url, string param)
+        {
+            if (url == null || param == null)
+                throw new ArgumentNullException();
+            if (url == "" || param == "")
+                throw new ArgumentException();
+            if (url.IndexOf('?') == -1)
+            {
+                url += "?" + param;
+            }
+            else
+            {
+                string key = param.Remove(param.IndexOf('='));
+                if (url.Substring(url.IndexOf("?") + 1, url.IndexOf("=") - url.IndexOf("?") - 1) == key)
+                {
+                    url = url.Remove(url.IndexOf('?') + 1, url.Length - url.IndexOf('?') - 1);
+                    url += param;
+
+                }
+                if (url.Substring(url.IndexOf("?") + 1, url.IndexOf("=") - url.IndexOf("?") - 1) != key)
+                {
+                    url += "&" + param;
+                }
+            }
+            return url;
+        }
+    }
+}
---
+++ Manipulate URL ParametersTests/ManipulateURLTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Manipulate_URL_Parameters;
+
+namespace Manipulate_URL_Parameters.Tests
+{
+    [TestFixture]
+    public class ManipulateURLTests
+    {
+        [Test]
+        public void addOrChangeUrlParameterTest()
+        {
+            Assert.AreEqual(ManipulateURL.addOrChangeUrlParameter("www.example.com", "key=value"), "www.example.com?key=value");
+            Assert.AreEqual(ManipulateURL.addOrChangeUrlParameter("www.example.com?key=value", "key2=value2"), "www.example.com?key=value&key2=value2");
+            Assert.AreEqual(ManipulateURL.addOrChangeUrlParameter("www.example.com?key=oldValue`", "key=newValue"), "www.example.com?key=newValue");
+        }
+
+        [Test]
+        public void ParametersIsNullOrEmpty()
+        {
+            Assert.Throws<ArgumentNullException>(delegate { ManipulateURL.addOrChangeUrlParameter(null, null); });
+            Assert.Throws<ArgumentException>(delegate { ManipulateURL.addOrChangeUrlParameter("", ""); });
+        }
+    }
+}
